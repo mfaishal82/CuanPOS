596bd35ae0e54da6530f2d336bc4297f50f96fb6
mfaishal82 <mfaishal82>
2025-12-09 07:13:29 +0700

    feat: category router

--- pos-backend/routes/categoryRouter.js
+++ pos-backend/routes/categoryRouter.js
@@ -1,8 +1,11 @@
 const express = require("express")
-const { getCategories } = require("../controllers/categoryController")
+const { getCategories, createCategory, getCategoryById, editCategoryById, deleteCategoryById } = require("../controllers/categoryController")
 const router = express.Router()
 
 router.get('/list', getCategories)
-// router.post('/add')
+router.post('/add', createCategory)
+router.get('/:id', getCategoryById)
+router.put('/:id', editCategoryById)
+router.delete('/:id', deleteCategoryById)
 
 module.exports = router
