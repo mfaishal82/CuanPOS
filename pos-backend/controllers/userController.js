7b3024cf569baf15632e3dc34d184a492044ff53
mfaishal82 <mfaishal82>
2025-12-11 08:27:29 +0700

    feat: cookie implemetation

--- pos-backend/controllers/userController.js
+++ pos-backend/controllers/userController.js
@@ -29,10 +29,13 @@ class UserController {
 
       res.cookie(
         'token', token, {
+          expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+          ),
           httpOnly: true,
           secure: false,
           sameSite: 'none',
-          // domain: '.aws.com'
+          // domain: 'mydomain.com' || '.vercel.app'
         }
       )
 
