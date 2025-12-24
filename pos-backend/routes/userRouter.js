c1dded9a818408180f926ef91ece8d39f32cd408
mfaishal82 <mfaishal82@yahoo.co.id>
2025-12-24 09:31:15 +0700

    feat: add edit & delete user by id

--- pos-backend/routes/userRouter.js
+++ pos-backend/routes/userRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { login, createUser, getUsers } = require("../controllers/userController");
+const { login, createUser, getUsers, editUserById, deleteUserById } = require("../controllers/userController");
 const isAdmin = require('../middlewares/isAdmin');
 
 router.post('/login', login)
 router.use(isAdmin)
 router.post('/register', createUser)
 router.get('/list-users', getUsers)
+router.put('/user/:id', editUserById)
+router.delete('/user/:id', deleteUserById)
 
 module.exports = router;
