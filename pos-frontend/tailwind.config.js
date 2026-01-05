eb8608ff3b3cbe271c53b909815dae4cf63c63a2
mfaishal82 <mfaishal82@yahoo.co.id>
2026-01-05 17:29:56 +0700

    refactor tailwind from CDN to default best practice in config

---
+++ pos-frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+export default {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#13ec80',
+        'background-light': '#f8fcfa',
+        'background-dark': '#102219',
+        'surface-light': '#ffffff',
+        'surface-dark': '#1a3326',
+        'text-main': '#0d1b14',
+        'text-muted': '#4c9a73',
+        'border-light': '#e7f3ed',
+        'border-dark': '#2a4a3b',
+      },
+      fontFamily: {
+        display: ['Inter', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+      },
+      borderRadius: {
+        DEFAULT: '0.25rem',
+        lg: '0.5rem',
+        xl: '0.75rem',
+        full: '9999px',
+      },
+    },
+  },
+}
