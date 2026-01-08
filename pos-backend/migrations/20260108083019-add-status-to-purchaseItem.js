b5b9ee77af71337db59dfc3352abf8d6d1cc0b09
mfaishal82 <mfaishal82@yahoo.co.id>
2026-01-08 15:31:57 +0700

    feat status purchase item :enum

---
+++ pos-backend/migrations/20260108083019-add-status-to-purchaseItem.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn("PurchaseItems", "status", {
+      type: Sequelize.ENUM("draft", "completed", "canceled"),
+      allowNull: false,
+      defaultValue: "pending",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
