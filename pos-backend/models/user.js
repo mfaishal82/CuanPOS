f8df4bf02706cf35981aa81e6a680868c7137f9d
mfaishal82 <mfaishal82@yahoo.co.id>
2026-02-28 11:13:37 +0700

    add library nodemailer

--- pos-backend/models/user.js
+++ pos-backend/models/user.js
@@ -1,8 +1,6 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-const { hashPassword } = require('../helpers/bcrypt');
+"use strict";
+const { Model } = require("sequelize");
+const { hashPassword } = require("../helpers/bcrypt");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -12,56 +10,66 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Purchase, { foreignKey: 'user_id' })
+      User.hasMany(models.Purchase, { foreignKey: "user_id" });
     }
   }
-  User.init({
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: { msg: "Username is required" },
-        notNull: { msg: "Username is required" }
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: { msg: 'Password is required' },
-        notNull: { msg: 'Password is required' },
-        len: {
-          args: [8, 14],
-          msg: 'Password length should be between 5 and 14 characters'
-        }
-      }
-    },
-    role: DataTypes.ENUM('admin', 'user'),
-    image: {
-      type: DataTypes.STRING,
-      defaultValue: 'https://ik.imagekit.io/myfiles/vecteezy_male-profile-picture-placeholder-for-social-media-forum_.jpg'
-    },
-    status: {
-      type: DataTypes.ENUM('active', 'inactive'),
-      defaultValue: 'active'
-    }
-  }, {
-    sequelize,
-    modelName: 'User',
-    hooks: {
-      beforeCreate: (user) => {
-        user.password = hashPassword(user.password)
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
-      beforeUpdate: (user) => {
-        if (user.changed('password')) {
-          user.password = hashPassword(user.password)
-        }
-      }
-    }
-  });
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Username is required" },
+          notNull: { msg: "Username is required" },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Password is required" },
+          notNull: { msg: "Password is required" },
+          len: {
+            args: [8, 14],
+            msg: "Password length should be between 5 and 14 characters",
+          },
+        },
+      },
+      role: DataTypes.ENUM("admin", "user"),
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: true,
+        },
+      },
+      image: {
+        type: DataTypes.STRING,
+        defaultValue:
+          "https://ik.imagekit.io/myfiles/vecteezy_male-profile-picture-placeholder-for-social-media-forum_.jpg",
+      },
+      status: {
+        type: DataTypes.ENUM("active", "inactive"),
+        defaultValue: "active",
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      hooks: {
+        beforeCreate: (user) => {
+          user.password = hashPassword(user.password);
+        },
+        beforeUpdate: (user) => {
+          if (user.changed("password")) {
+            user.password = hashPassword(user.password);
+          }
+        },
+      },
+    },
+  );
   return User;
 };
