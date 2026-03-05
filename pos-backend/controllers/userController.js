03a5bfe5ddf4e207b580ef5b4846c65e846548b4
mfaishal82 <mfaishal82@yahoo.co.id>
2026-03-05 08:59:10 +0700

    feat backend getuser by id

--- pos-backend/controllers/userController.js
+++ pos-backend/controllers/userController.js
@@ -1,77 +1,75 @@
-const { comparePassword, hashPassword } = require('../helpers/bcrypt');
-const { signToken } = require('../helpers/jwt');
-const { User } = require('../models');
-const { Op } = require('sequelize')
+const { comparePassword, hashPassword } = require("../helpers/bcrypt");
+const { signToken } = require("../helpers/jwt");
+const { User } = require("../models");
+const { Op } = require("sequelize");
 
 class UserController {
   static async login(req, res, next) {
     try {
-      const { username, password } = req.body
+      const { username, password } = req.body;
       // console.log(req.body)
 
       if (!username || !password) {
         return res.status(400).json({
-          message: "Username and password are required"
-        })
+          message: "Username and password are required",
+        });
       }
 
       let user = await User.findOne({
         where: {
-          username
-        }
-      })
+          username,
+        },
+      });
 
-      if(!user || !comparePassword(password, user.password)) {
+      if (!user || !comparePassword(password, user.password)) {
         return res.status(401).json({
-          message: "Invalid username or password"
-        })
+          message: "Invalid username or password",
+        });
       }
 
       const token = signToken({
-        id:user.id,
+        id: user.id,
         username: user.username,
-      })
+      });
 
-      res.cookie(
-        'token', token, {
-          expires: new Date(
-            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-          ),
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-          path: '/',
-          // maxAge: process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-          // domain: 'mydomain.com' || '.vercel.app'
-        }
-      )
+      res.cookie("token", token, {
+        expires: new Date(
+          Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+        ),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        path: "/",
+        // maxAge: process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        // domain: 'mydomain.com' || '.vercel.app'
+      });
 
       res.status(200).json({
         name: user.name,
         username: user.username,
-        role: user.role
-      })
+        role: user.role,
+      });
     } catch (error) {
       // res.status(500).json({message: "error test"})
       next(error);
     }
   }
 
-  static async logout(req, res, next){
-    try{
-      res.clearCookie('token', {
+  static async logout(req, res, next) {
+    try {
+      res.clearCookie("token", {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        path: '/',
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        path: "/",
         // domain: process.env.FRONTEND_URL
-      })
+      });
 
       res.status(200).json({
-        message: "Logout success"
-      })
-    }catch(error){
-      next(error)
+        message: "Logout success",
+      });
+    } catch (error) {
+      next(error);
     }
   }
 
@@ -81,8 +79,11 @@ class UserController {
 
       // console.log(req.body)
       const newUser = await User.create({
-        name, username, password, role
-      })
+        name,
+        username,
+        password,
+        role,
+      });
 
       // console.log(newUser)
 
@@ -90,105 +91,131 @@ class UserController {
         id: newUser.id,
         name: newUser.name,
         username: newUser.username,
-        role: newUser.role
-      })
+        role: newUser.role,
+      });
     } catch (error) {
       next(error);
     }
   }
 
   static async getMe(req, res, next) {
-    try{
-      const id = req.user.id
-      if(!id) throw { name: "NotFound" }
+    try {
+      const id = req.user.id;
+      if (!id) throw { name: "NotFound" };
 
       const user = await User.findByPk(id, {
-        attributes: ['id', 'name', 'username', 'role', 'image', 'status']
-      })
-      if(!user) throw { name: "NotFound" }
+        attributes: ["id", "name", "username", "role", "image", "status"],
+      });
+      if (!user) throw { name: "NotFound" };
       // console.log(user)
 
-      res.status(200).json(user)
-    }catch(error){
-      next(error)
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
     }
   }
 
-  static async getUsers(req, res, next){
-    try{
-      let { page, limit, searchUser } = req.query
-      page = parseInt(page) || 1
-      limit = parseInt(limit) || 10
-      let offset = (page - 1) * limit
+  static async getUsers(req, res, next) {
+    try {
+      let { page, limit, searchUser } = req.query;
+      page = parseInt(page) || 1;
+      limit = parseInt(limit) || 10;
+      let offset = (page - 1) * limit;
 
-      let option = searchUser ? {
-        [Op.or]: [{
-          name: {
-            [Op.iLike]: `%${searchUser}%`
+      let option = searchUser
+        ? {
+            [Op.or]: [
+              {
+                name: {
+                  [Op.iLike]: `%${searchUser}%`,
+                },
+              },
+              {
+                username: {
+                  [Op.iLike]: `%${searchUser}%`,
+                },
+              },
+            ],
           }
-        }, {
-          username: {
-            [Op.iLike]: `%${searchUser}%`
-          }
-        }]
-      } : {}
+        : {};
 
       const { count, rows } = await User.findAndCountAll({
         attributes: {
-          exclude: ['password']
+          exclude: ["password"],
         },
         where: option,
         offset,
-        limit
-      })
+        limit,
+      });
 
       res.status(200).json({
-        message: `Success get ` + searchUser ? searchUser : 'all users',
+        message: `Success get ` + searchUser ? searchUser : "all users",
         pagination: {
           page,
           limit,
           total: count,
-          totalPages: Math.ceil(count / limit)
+          totalPages: Math.ceil(count / limit),
         },
-        data: rows
-      })
-    }catch(error){
-      next(error)
+        data: rows,
+      });
+    } catch (error) {
+      next(error);
     }
   }
 
-  static async editUserById(req, res, next){
-    try{
-      const id = req.params.id
-      const { name, username, password, status, role } = req.body
+  static async getUserById(req, res, next) {
+    try {
+      const id = req.params.id;
 
-      const user = await User.findByPk(id)
-      if(!user) throw { name: "NotFound" }
+      if (!id) {
+        throw { name: "NotFound" };
+      }
+      const user = await User.findByPk(id, {
+        attributes: ["id", "name", "username", "role", "image", "status"],
+      });
 
-      await user.update({ name, username, password, status, role })
+      if (!user) {
+        throw { name: "NotFound" };
+      }
+
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async editUserById(req, res, next) {
+    try {
+      const id = req.params.id;
+      const { name, username, password, status, role } = req.body;
+
+      const user = await User.findByPk(id);
+      if (!user) throw { name: "NotFound" };
+
+      await user.update({ name, username, password, status, role });
 
       res.status(200).json({
-        message: `Success update data for ${user.username}`
-      })
-    }catch(error){
-      next(error)
+        message: `Success update data for ${user.username}`,
+      });
+    } catch (error) {
+      next(error);
     }
   }
 
   static async deleteUserById(req, res, next) {
-    try{
-      const id = req.params.id
+    try {
+      const id = req.params.id;
 
-      const user = await User.findByPk(id)
-      if(!user) throw { name: "NotFound" }
+      const user = await User.findByPk(id);
+      if (!user) throw { name: "NotFound" };
 
-      await user.destroy()
+      await user.destroy();
 
       res.status(200).json({
-        message: "Delete user success"
-      })
-    }catch(error){
-      next(error)
+        message: "Delete user success",
+      });
+    } catch (error) {
+      next(error);
     }
   }
 }
