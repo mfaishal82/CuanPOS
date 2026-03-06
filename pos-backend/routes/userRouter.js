00bdcafac19f8e39656a1033c1e91c47f23abf5a
mfaishal82 <mfaishal82@yahoo.co.id>
2026-03-06 11:01:55 +0700

    feat backend route getuser by id

--- pos-backend/routes/userRouter.js
+++ pos-backend/routes/userRouter.js
@@ -1,16 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { login, createUser, getUsers, editUserById, deleteUserById, logout, getMe } = require("../controllers/userController");
-const isAdmin = require('../middlewares/isAdmin');
-const Authentication = require('../middlewares/authentication');
+const {
+  login,
+  createUser,
+  getUsers,
+  editUserById,
+  deleteUserById,
+  logout,
+  getMe,
+  getUserById,
+} = require("../controllers/userController");
+const isAdmin = require("../middlewares/isAdmin");
+const Authentication = require("../middlewares/authentication");
 
-router.post('/login', login)
-router.post('/logout', Authentication, logout)
-router.get('/getme', Authentication, getMe)
-router.use(isAdmin)
-router.post('/register', createUser)
-router.get('/list-users', getUsers)
-router.put('/user/:id', editUserById)
-router.delete('/user/:id', deleteUserById)
+router.post("/login", login);
+router.post("/logout", Authentication, logout);
+router.get("/getme", Authentication, getMe);
+router.use(isAdmin);
+router.post("/register", createUser);
+router.get("/list-users", getUsers);
+router.get("/user/:id", getUserById);
+router.put("/user/:id", editUserById);
+router.delete("/user/:id", deleteUserById);
 
 module.exports = router;
